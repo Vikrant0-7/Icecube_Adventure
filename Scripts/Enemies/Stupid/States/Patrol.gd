0b976c73841072f2160ce307c95142535d72d9e9
VickyMotasara <vickymotasara@gmail.com>
2021-08-30 17:39:30 +0530

    fixed glitch where enemy is stuck at wall

--- Scripts/Enemies/Stupid/States/Patrol.gd
+++ Scripts/Enemies/Stupid/States/Patrol.gd
@@ -1,12 +1,14 @@
 extends StupidState
 
 func start() -> void:
-	Self.dir.x = 1
+	Self.dir.x = sign(get_tree().get_nodes_in_group("Player")[0].global_position.x - Self.global_position.x)
 
 func fixed_update(delta: float) -> void:
 	if not Self.is_stupid:
 		if can_fall():
 			switch_direction()
+	if Self.is_on_wall():
+		switch_direction()
 	Self.velocity.y += 500 * delta
 	Self.velocity.x = Self.dir.x * 100
 	Self.velocity = Self.move_and_slide(Self.velocity, Vector2.UP)
